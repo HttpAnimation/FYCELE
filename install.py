bce9baa06beaa090a8f95d85e13f7f52d59b6199
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-06-17 14:33:42 -0700

    cleaner install.py

--- install.py
+++ install.py
@@ -1,15 +1,18 @@
 import os
 import time
+
 print("Installing and setting up event")
 time.sleep(5)
 os.system("sudo apt install")
 os.system("npm init -y")
 os.system("npm install electron")
-print("Removeing unsed files")
+
+print("Removing unused files")
 time.sleep(5)
 os.system("rm main.js")
 os.system("rm package-lock.json")
-os.system("package.json")
+os.system("rm package.json")
+
 print("Adding files")
 time.sleep(5)
 os.system("wget https://raw.githubusercontent.com/HttpAnimation/FYCELE/main/main.js")
@@ -17,8 +20,10 @@ os.system("wget https://raw.githubusercontent.com/HttpAnimation/FYCELE/main/pack
 os.system("wget https://raw.githubusercontent.com/HttpAnimation/FYCELE/main/package.json")
 os.system("wget https://raw.githubusercontent.com/HttpAnimation/FYCELE/main/start.py")
 os.system("mkdir FYC")
-print("Put your fyc files in the folder FYC on the root dir NO SUB FOLDER it should look like this")
+
+print("Put your FYC files in the folder FYC on the root directory. The folder structure should look like this:")
 print("FYC@linux:~/Downloads/AWAWAW$ ls")
 print("FYC  install.py  main.js  node_modules  package.json  package.json.1  package-lock.json")
+
 time.sleep(5)
-print("Once you put in the FYC files run start.py to start or run | npm start")
+print("Once you put in the FYC files, run start.py to start or run 'npm start'.")
